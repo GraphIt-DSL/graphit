1bdf1466b62cc4e6536a3fd421b6d063bb0caa06
Yunming Zhang <zhangyunming1990@gmail.com>
2017-06-08 21:59:15 -0400

    set up high level and low level schedule tests

---
+++ test/c++/low_level_schedule_test.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Yunming Zhang on 6/8/17.
+//
+
+#include <gtest.h>
+#include <graphit/frontend/frontend.h>
+#include <graphit/midend/mir_context.h>
+#include <graphit/midend/midend.h>
+#include <graphit/backend/backend.h>
+#include <graphit/frontend/error.h>
+#include <graphit/utils/exec_cmd.h>
+
+using namespace std;
+using namespace graphit;
+
+class LowLevelScheduleTest : public ::testing::Test {
+protected:
+    virtual void SetUp(){
+        context_ = new graphit::FIRContext();
+        errors_ = new std::vector<ParseError>();
+        fe_ = new Frontend();
+        mir_context_  = new graphit::MIRContext();
+
+    }
+
+    virtual void TearDown() {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+
+        //prints out the MIR, just a hack for now
+        //std::cout << "mir: " << std::endl;
+        //std::cout << *(mir_context->getStatements().front());
+        //std::cout << std::endl;
+
+    }
+
+    bool basicTest(std::istream & is){
+        fe_->parseStream(is, context_, errors_);
+        graphit::Midend* me = new graphit::Midend(context_);
+
+        std::cout << "fir: " << std::endl;
+        std::cout << *(context_->getProgram());
+        std::cout << std::endl;
+
+        me->emitMIR(mir_context_);
+        graphit::Backend* be = new graphit::Backend(mir_context_);
+        return be->emitCPP();
+    }
+
+
+    bool basicTestWithSchedule(std::istream & is, Schedule* schedule){
+        fe_->parseStream(is, context_, errors_);
+        graphit::Midend* me = new graphit::Midend(context_, schedule);
+        std::cout << "fir: " << std::endl;
+        std::cout << *(context_->getProgram());
+        std::cout << std::endl;
+
+        me->emitMIR(mir_context_);
+        graphit::Backend* be = new graphit::Backend(mir_context_);
+        return be->emitCPP();
+    }
+
+    std::vector<ParseError> * errors_;
+    graphit::FIRContext* context_;
+    Frontend * fe_;
+    graphit::MIRContext* mir_context_;
+};
