61227ed3502d0cfdea55c202e0e3a55e3e586468
Yunming Zhang <zhangyunming1990@gmail.com>
2017-06-13 13:30:17 -0400

    adding appendloopboy, remove label support, getting simple loop index split to work

--- test/c++/test.cpp
+++ test/c++/test.cpp
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
 
     //::testing::GTEST_FLAG(filter) = "RuntimeLibTest.TimerTest";
 
-    ::testing::GTEST_FLAG(filter) = "LowLevelScheduleTest.SimpleInsertForLoopNodeBefore";
+    //::testing::GTEST_FLAG(filter) = "LowLevelScheduleTest.SimpleLoopIndexSplit";
 
 
     return RUN_ALL_TESTS();
