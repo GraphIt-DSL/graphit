474f96d4e60214e1175b5db93e0422d2d5915185
Yunming Zhang <zhangyunming1990@gmail.com>
2017-06-28 11:49:08 -0400

    adding plans and comments for building parallelism

--- src/midend/mir_lower.cpp
+++ src/midend/mir_lower.cpp
@@ -17,6 +17,18 @@ namespace graphit {
         VectorOpLower(mir_context).lower();
         // This pass lowers apply expression into concrete versions, including push, pull hybrid and more
         ApplyExprLower(mir_context, schedule).lower();
+
+        // This pass adds parallel labels to vertexset and edgeset operations
+
+        // This pass fuses conditions and applies to prepare for later generating CAS operations
+        // for parallel edgeset with from to filtering
+
+        // This pass inserts atomic operations including CAS, writeMin, writeAdd
+
+        // This pass generates return values for implicit tracking of changes to certain field
+        
+
+
         // The pass on lowering abstract data structures to
         // concrete data structures with physical layout information
         PhysicalDataLayoutLower(mir_context, schedule).lower();
