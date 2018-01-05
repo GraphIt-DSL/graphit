afae6c600bde53fcd966a9ef9fdbec59106d8807
Yunming Zhang <zhangyunming1990@gmail.com>
2018-01-05 15:34:12 -0500

    updating the test cases with separate algo and schedule files

--- test/python/test_with_schedules.py
+++ test/python/test_with_schedules.py
@@ -51,6 +51,25 @@ class TestGraphitCompiler(unittest.TestCase):
         if os.path.isfile(self.executable_file_name):
             os.remove(self.executable_file_name)
 
+
+    # compiles the program with a separate input algorithm file and input schedule file
+    # allows us to unit test various different schedules with the same algorithm
+    def basic_compile_test_with_separate_algo_schedule_files(self, input_algo_file, input_schedule_file):
+        input_algos_path = '../../test/input/'
+        input_schedules_path = '../../test/input_with_schedules/'
+        print os.getcwd()
+        algo_file = input_algos_path + input_algo_file
+        schedule_file = input_schedules_path + input_schedule_file
+        compile_cmd = "python graphitc.py -a " + algo_file + " -f " + schedule_file + " -o test.cpp"
+        print compile_cmd
+        subprocess.check_call(compile_cmd, shell=True)
+        cpp_compile_cmd = "g++ -g -std=c++11 -I ../../src/runtime_lib/  test.cpp -o test.o"
+        if use_parallel:
+            print "using icpc for parallel compilation"
+            cpp_compile_cmd = "icpc -g -std=c++11 -I ../../src/runtime_lib/ -DCILK test.cpp -o test.o"
+        subprocess.check_call(cpp_compile_cmd, shell=True)
+
+    # compiles the input file with both the algorithm and schedule specification
     def basic_compile_test(self, input_file_name):
         input_with_schedule_path = '../../test/input_with_schedules/'
         print os.getcwd()
@@ -130,8 +149,11 @@ class TestGraphitCompiler(unittest.TestCase):
         self.assertEqual(test_flag, True)
         os.chdir("bin")
 
-    def pr_verified_test(self, input_file_name):
-        self.basic_compile_test(input_file_name)
+    def pr_verified_test(self, input_file_name, use_separate_algo_file=False):
+        if (use_separate_algo_file):
+            self.basic_compile_test_with_separate_algo_schedule_files("pagerank_with_filename_arg.gt", input_file_name)
+        else:
+            self.basic_compile_test(input_file_name)
         proc = subprocess.Popen("./"+ self.executable_file_name + " ../../test/graphs/test.el", shell=True, stdout=subprocess.PIPE)
         #check the value printed to stdout is as expected
         output = proc.stdout.readline()
@@ -167,11 +189,13 @@ class TestGraphitCompiler(unittest.TestCase):
         print "output: " + output.strip()
         self.assertEqual(float(output.strip()), 7.49039)
 
+
     def test_simple_splitting(self):
         self.basic_compile_test("simple_loop_index_split.gt")
 
     def test_pagerank_AoS(self):
-        self.basic_compile_test("simple_pagerank_with_AoS.gt")
+        #self.basic_compile_test("simple_pagerank_with_AoS.gt")
+        self.basic_compile_test_with_separate_algo_schedule_files("pagerank.gt", "simple_pagerank_with_AoS.gt")
 
     def test_eigenvector_pagerank_fusion(self):
         self.basic_compile_test("eigenvector_pr_fusion.gt")
@@ -220,7 +244,7 @@ class TestGraphitCompiler(unittest.TestCase):
         self.sssp_verified_test("sssp_push_parallel_sliding_queue.gt")
 
     def test_pagerank_parallel_pull_expect(self):
-        self.pr_verified_test("pagerank_pull_parallel.gt")
+        self.pr_verified_test("pagerank_pull_parallel.gt", True)
 
     def test_pagerank_parallel_push_expect(self):
         self.pr_verified_test("pagerank_push_parallel.gt")
@@ -256,5 +280,6 @@ if __name__ == '__main__':
 
     # used for enabling a specific test
     # suite = unittest.TestSuite()
-    # suite.addTest(TestGraphitCompiler('test_sssp_push_parallel_sliding_queue_verified'))
+    # suite.addTest(TestGraphitCompiler('test_pagerank_AoS'))
+    # suite.addTest(TestGraphitCompiler('test_pagerank_parallel_pull_expect'))
     # unittest.TextTestRunner(verbosity=2).run(suite)
