513612122317f443ccd06783cee91ad372920067
Yunming Zhang <zhangyunming1990@gmail.com>
2018-02-02 16:54:20 -0500

    updating the compile scripts to get ready for autotune

--- autotune/pagerank_tune.py
+++ autotune/pagerank_tune.py
@@ -3,7 +3,7 @@
 # Autotune schedules for PageRank in the GraphIt language
 #                                                                               
 
-import adddeps  # fix sys.path
+# import adddeps  # fix sys.path
 import opentuner
 from opentuner import ConfigurationManipulator
 from opentuner import EnumParameter
@@ -12,17 +12,45 @@ from opentuner import MeasurementInterface
 from opentuner import Result
 
 
+
+
 class GraphItPageRankTuner(MeasurementInterface):
     def manipulator(self):
         """                                                                          
         Define the search space by creating a                                        
         ConfigurationManipulator                                                     
         """
+        manipulator = ConfigurationManipulator()
+        manipulator.add_parameter(
+            EnumParameter('direction', 
+                          ['push','pull','hybrid_dense']))
+
+        return manipulator
 
     def compile(self, cfg, id):
         """                                                                          
         Compile a given configuration in parallel                                    
         """
+        
+        #write into a schedule file the configuration
+        f = open('schedules/default_schedule.gt','r')
+        default_schedule_str = f.read()
+        f.close()
+        new_schedule = default_schedule_str.replace('$direction', cfg['direction'])
+        print (new_schedule)
+        new_schedule_file_name = 'schedule_' + str(id) 
+        print (new_schedule_file_name)
+        f1 = open (new_schedule_file_name, 'w')
+        f1.write(new_schedule)
+        f1.close()
+        
+        compile_graphit_cmd = 'python ../src/graphitc.py -a apps/pagerank_benchmark.gt -f ' + new_schedule_file_name + ' -o test.cpp' 
+        compile_cpp_cmd = 'icpc -std=c++11 -I ../src/runtime_lib/ -O3 -DCILK test.cpp -o test'
+        print(compile_graphit_cmd)
+        print(compile_cpp_cmd)
+        self.call_program(compile_graphit_cmd)
+        return self.call_program(compile_cpp_cmd)
+        
     
     def run_precompiled(self, desired_result, input, limit, compile_result, id):
         """                                                                          
@@ -38,6 +66,6 @@ class GraphItPageRankTuner(MeasurementInterface):
         compile_result = self.compile(cfg, 0)
         return self.run_precompiled(desired_result, input, limit, compile_result, 0)
 
-    if __name__ == '__main__':
-        argparser = opentuner.default_argparser()
-        GccFlagsTuner.main(argparser.parse_args())
+if __name__ == '__main__':
+    argparser = opentuner.default_argparser()
+    GraphItPageRankTuner.main(argparser.parse_args())
--- src/graphitc.py
+++ src/graphitc.py
@@ -8,6 +8,8 @@ def parseArgs():
     parser.add_argument('-f', dest = 'input_file_name')
     parser.add_argument('-o', dest = 'output_file_name')
     parser.add_argument('-a', dest = 'input_algo_file_name')
+    parser.add_argument('-i', dest = 'runtime_include_path', default = '../../include/')
+    parser.add_argument('-l', dest = 'graphitlib_path', default = '../lib/libgraphitlib.a')
     args = parser.parse_args()
     return vars(args)
 
@@ -15,6 +17,8 @@ if __name__ == '__main__':
     args = parseArgs()
     input_file_name = args['input_file_name']
     output_file_name = args['output_file_name']
+    runtime_include_path = args['runtime_include_path']
+    graphitlib_path = args['graphitlib_path']
     compile_template_file = "main.cpp"
 
     #check if user supplied a separate algorithm file from the schedule file
@@ -28,6 +32,7 @@ if __name__ == '__main__':
         # use the input_file for both the algorithm and schedule
         algo_file_name = 'algo.gt'
     compile_file_name = 'compile.cpp'
+
 
     # read the input file
     with open(input_file_name) as f:
@@ -72,7 +77,6 @@ if __name__ == '__main__':
     # compile and execute compile.cpp file to complete the compilation
     #TODO: code here uses very fragile relavtive paths, figure out a better way
     # Maybe setting environment variables
-    subprocess.check_call("g++ -g -std=c++11 -I ../../include/ " + compile_file_name
-                          + " -o compile.o ../lib/libgraphitlib.a", shell=True)
+    subprocess.check_call("g++ -g -std=c++11 -I {0} {1} -o compile.o {2}".format(runtime_include_path, compile_file_name, graphitlib_path), shell=True)
     subprocess.check_call("./compile.o  -f " + algo_file_name +  " -o " + output_file_name, shell=True)
     #subprocess.check_call("g++ -g -std=c++11 -I ../../src/runtime_lib/  " + output_file_name + " -o test.o", shell=True)
