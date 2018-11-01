5c170fde2ba023a775b720c19d01c0727e0ac7d9
zhangyunming1990@gmail.com <zhangyunming1990@gmail.com>
2018-10-31 23:51:13 -0400

    updating the evaluation scripts to generate cpps files from the .gt files

--- graphit_eval/eval/table7/benchmark.py
+++ graphit_eval/eval/table7/benchmark.py
@@ -6,7 +6,7 @@ import shlex
 import time
 from threading import Timer
 
-use_NUMACTL = True
+use_NUMACTL = True 
 
 
 framework_app_lookup = {
@@ -68,7 +68,9 @@ def path_setup(frameworks):
 
 def get_starting_points(graph):
     """ Use the points with non-zero out degree and don't hang during execution.  """
-    if graph != "friendster":
+    if graph == "testGraph":
+        return ["1","2"]
+    elif graph != "friendster":
         return ["17", "38", "47", "52", "53", "58", "59", "69", "94", "96"]
     else:
         # friendster takes a long time so use fewer starting points
@@ -93,7 +95,7 @@ def get_cmd_graphit(g, p, point):
 
     command = graphit_PATH + graphit_binary_map[g][p] + " " + args
     #if p in ["pr", "cc", "prd"] and g in ["twitter", "webGraph", "friendster"]:
-    if g in ["twitter", "webGraph", "friendster"]:
+    if g in ["socLive", "twitter", "webGraph", "friendster"]:
         
         if use_NUMACTL:
             # if NUAMCTL is available
@@ -118,11 +120,11 @@ def main():
     parser.add_argument('-a', '--applications', nargs='+',
                         default=["bfs", "sssp", "pr", "cc", "prd"], 
                         help="applications to benchmark. Defaults to all four applications.")
-    parser.add_argument('--use_NUMACTL', type=int, default=1, help='use numactl when running. 1 for enable (default), 0 for disable')
+    
     args = parser.parse_args()
 
-    if args.use_NUMACTL != 1:
-        use_NUMACTL = False
+
+
 
     path_setup(["graphit"])
 
