125bd5a78872bbab5c1e674597448bb2e4af4c84
Yunming <yunmingzhang@dhcp-18-30-40-60.dyn.mit.edu>
2019-03-12 00:04:08 -0400

    adding a simple sssp test with ordered processing operator

--- test/c++/runtime_lib_test.cpp
+++ test/c++/runtime_lib_test.cpp
@@ -5,6 +5,8 @@
 #include <gtest.h>
 #include "intrinsics.h"
 #include "infra_gapbs/graph_verifier.h"
+#include "infra_gapbs/ordered_processing.h"
+
 
 
 
@@ -88,26 +90,62 @@ TEST_F(RuntimeLibTest, VertexSubsetSimpleTest) {
 //test init of the eager priority queue based on GAPBS
 TEST_F(RuntimeLibTest, EagerPriorityQueueInit) {
 
-    const WeightT kDistInf = numeric_limits<WeightT>::max()/2;
+    const WeightT kDistInf = std::numeric_limits<WeightT>::max()/2;
 
-    Graph g = builtin_loadEdgesFromFile("../../test/graphs/test.el");
+    WGraph g = builtin_loadWeightedEdgesFromFile("../../test/graphs/test.el");
     WeightT* dist_array = new WeightT[g.num_nodes()];
     for (int i = 0; i < g.num_nodes(); i++){
         dist_array[i] = kDistInf;
     }
-    EagerPriorityQueue<WeightT>* pq = new EagerPriorityQueue<WeightT>(dist_array);
+    EagerPriorityQueue<WeightT> pq = EagerPriorityQueue<WeightT>(dist_array);
 
-    EXPECT_EQ(pq->get_current_priority(), 0);
+    EXPECT_EQ(pq.get_current_priority(), 0);
 }
 
 //test init of the buffered priority queue based on Julienne
 TEST_F(RuntimeLibTest, BufferedPriorityQueueInit) {
+
 
     EXPECT_EQ(true, true);
 }
 
 // test compilation of the C++ version of SSSP using eager priority queue
 TEST_F(RuntimeLibTest, SSSP_test){
+
+
+
+    const WeightT kDistInf = std::numeric_limits<WeightT>::max()/2;
+
+    WGraph g = builtin_loadWeightedEdgesFromFile("../../test/graphs/test.el");
+    WeightT* dist_array = new WeightT[g.num_nodes()];
+    for (int i = 0; i < g.num_nodes(); i++){
+        dist_array[i] = kDistInf;
+    }
+
+    NodeID source = 2;
+    dist_array[source] = 0;
+
+    EagerPriorityQueue<WeightT> pq = EagerPriorityQueue<WeightT>(dist_array);
+    WeightT input_delta = 2;
+
+    auto src_filter_func = [&](NodeID v)-> bool {
+        return (dist_array[v] >= input_delta * static_cast<WeightT>(pq.get_current_priority()));
+    };
+
+    auto while_cond_func = [&]()->bool{
+            return !pq.finished();
+    };
+
+
+    auto edge_update_func = [&](vector<vector<NodeID> >& local_bins, NodeID src, NodeID dst, WeightT wt) {
+        WeightT old_dist = dist_array[dst];
+        WeightT new_dist = dist_array[src] + wt;
+        update_priority_min<WeightT>()(&pq, local_bins, dst, old_dist, new_dist);
+    };
+
+    OrderedProcessingOperatorWithMerge(&pq, g, dist_array, src_filter_func, while_cond_func, edge_update_func, 1000,  source);
+
+    EXPECT_EQ(true, true);
 
 }
 
