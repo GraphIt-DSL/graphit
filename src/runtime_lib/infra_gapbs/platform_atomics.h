7fc8303e2bb4e9bb3d0c49febcef11bb2f0af1fe
Yunming <yunmingzhang@30-10-136.wireless.csail.mit.edu>
2019-03-15 20:49:52 -0400

    refactoring the OrderedProcessingOperator, fix atomics redefinition issue

--- src/runtime_lib/infra_gapbs/platform_atomics.h
+++ src/runtime_lib/infra_gapbs/platform_atomics.h
@@ -27,19 +27,18 @@ Wrappers for compiler intrinsics for atomic memory operations (AMOs)
     }
 
     template<typename T>
-    bool compare_and_swap(T &x, const T &old_val, const T &new_val) {
+    static bool compare_and_swap(T &x, const T &old_val, const T &new_val) {
       return __sync_bool_compare_and_swap(&x, old_val, new_val);
     }
 
-    template<>
-    bool compare_and_swap(float &x, const float &old_val, const float &new_val) {
+    
+    static bool compare_and_swap(float &x, const float &old_val, const float &new_val) {
       return __sync_bool_compare_and_swap(reinterpret_cast<uint32_t*>(&x),
                                           reinterpret_cast<const uint32_t&>(old_val),
                                           reinterpret_cast<const uint32_t&>(new_val));
     }
 
-    template<>
-    bool compare_and_swap(double &x, const double &old_val, const double &new_val) {
+    static bool compare_and_swap(double &x, const double &old_val, const double &new_val) {
       return __sync_bool_compare_and_swap(reinterpret_cast<uint64_t*>(&x),
                                           reinterpret_cast<const uint64_t&>(old_val),
                                           reinterpret_cast<const uint64_t&>(new_val));
