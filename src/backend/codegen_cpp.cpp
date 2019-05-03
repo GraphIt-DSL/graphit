5c2e92df1b362eed80d51e21a907986e2a8e546c
kenny yang <ykenny@mit.edu>
2019-05-03 03:50:01 -0400

    argvsafe complete with test

--- src/backend/codegen_cpp.cpp
+++ src/backend/codegen_cpp.cpp
@@ -622,12 +622,17 @@ namespace graphit {
  */
     void CodeGenCPP::visit(mir::TensorArrayReadExpr::Ptr expr) {
         //for dense array tensor read
-
+//        expr->target->accept(this);
+//        oss << "[";
+//        expr->index->accept(this);
+//        oss << "]";
+//    }
         if (mir::isa<mir::MIRNode>(expr.get()->target.get()->shared_from_this())) {
             //not sure what this is std::shared_ptr<mir::MIRNode> ptr = expr.get()->target.get()->shared_from_this();
             std::string nameptr = expr.get()->getTargetNameStr();
             if (nameptr == "argv"){
-                oss << "argv_safe(";
+                expr->target->accept(this);
+                oss << "_safe(";
                 expr->index->accept(this);
                 oss << ", argv, argc)";
             }
