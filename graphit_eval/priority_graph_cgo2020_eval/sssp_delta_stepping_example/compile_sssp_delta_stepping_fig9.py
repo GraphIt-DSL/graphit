54ac662617fbdc31ff2e848ffa593c3ed91714fe
Yunming Zhang <zhangyunming1990@gmail.com>
2019-11-08 17:29:15 -0500

    adding more schedules for the artifact evaluation

--- graphit_eval/priority_graph_cgo2020_eval/sssp_delta_stepping_example/compile_sssp_delta_stepping_fig9.py
+++ graphit_eval/priority_graph_cgo2020_eval/sssp_delta_stepping_example/compile_sssp_delta_stepping_fig9.py
@@ -7,12 +7,12 @@ import subprocess
 import os
 import sys
 
-algo_file = "../../apps/sssp_delta_stepping.gt"
-build_directory = "../../../build/bin"
+algo_file = "../../../apps/sssp_delta_stepping.gt"
+build_directory = "../../../build/bin/"
 
 # relative path from build_directory
-input_schedules_path = '../../graphit_eval/priority_graph_cgo2020_eval/sssp_delta_stepping_example/schedules/'
-sssp_demo_dir = '../../graphit_eval/priority_graph_cgo2020_eval/sssp_delta_stepping_example'
+input_schedules_path = './schedules/'
+sssp_demo_dir = './'
 
 def myprint(message):
     print ("----------------------------------------------------------------")
@@ -23,14 +23,13 @@ def myprint(message):
 def compile_and_myprint(input_schedule_file,  message, cpp_filename):
     # compiles the program with a separate input algorithm file and input schedule file
     # the relative schedule path from build directory 
-    os.chdir(build_directory)
     myprint ("current directory: " + os.getcwd())
     schedule_file = input_schedules_path + input_schedule_file
-    compile_cmd = "python graphitc.py -a " + algo_file + " -f " + schedule_file + " -o " + cpp_filename
+    compile_cmd = "python " + build_directory +  "/graphitc.py -a " + algo_file + " -f " + schedule_file + " -o " + cpp_filename
     myprint (compile_cmd)
     subprocess.check_call(compile_cmd, shell=True)
     
-    myprint ("PageRankDelta algo file with schedule %s compiled successfully! " % input_schedule_file)
+    myprint ("SSSP Delta Stepping algo file with schedule %s compiled successfully! " % input_schedule_file)
 
     myprint (message)
     cat_cmd = "cat " + cpp_filename
@@ -38,7 +37,7 @@ def compile_and_myprint(input_schedule_file,  message, cpp_filename):
     subprocess.check_call(cat_cmd, shell=True)
 
     myprint ("Generated c++ file saved in: " + cpp_filename)
-    cpp_compile_cmd = "g++ -g -std=gnu++1y -I ../../src/runtime_lib/ " + cpp_filename +  " -o test.o"
+    cpp_compile_cmd = "g++ -g -std=gnu++1y -I ../../../src/runtime_lib/ " + cpp_filename +  " -o test.o"
     
     myprint (cpp_compile_cmd)
     subprocess.check_call(cpp_compile_cmd, shell=True)
@@ -46,26 +45,24 @@ def compile_and_myprint(input_schedule_file,  message, cpp_filename):
 
 
 def eval_schedules():
-    myprint("Reproducing Fig.9: Generating different SSSP Delta Stepping C++ implemenataion with different schedules. Schedules are stored in the ./schedules directory. The algorithm file (sssp_delta_stepping.gt) is in graphit/apps directiory. ")
+    myprint("Reproducing schedules in Fig.9 and even more complicated schedules: Generating different SSSP Delta Stepping C++ implemenataion with different schedules. Schedules are stored in the ./schedules directory. The algorithm file (sssp_delta_stepping.gt) is in graphit/apps directiory. ")
 
-    # Fig 6(a) The default schedule
-    compile_and_myprint("SparsePush_VertexParallel_Delta2.gt", "SparsePush VertexParallel schedule Fig 6(a)", sssp_demo_dir + "sssp_delta_stepping_SparsePush_VertexParallel_schedule.cpp")
+    #os.chdir(build_directory)
 
-    exit()
+    # Fig 9(a) Schedule with Lazy Bucket Update, Sparsepush direction
+    compile_and_myprint("SparsePush_VertexParallel_Delta2.gt", "SparsePush VertexParallel schedule Fig 9(a)", sssp_demo_dir + "sssp_SparsePush_VertexParallel_schedule.cpp")
 
-    # Fig 6(b) Schedule with Direction Configured
-    compile_and_myprint("direction_schedule.gt", "Schedule with Direction Configured Fig 6(b)", pagerank_delta_demo_dir + "pagerankdelta_direction_schedule.cpp")
+    # Fig 9(b) Schedule with Lazy Bucket Update, DensePull direction
+    compile_and_myprint("DensePull_VertexParallel_Delta2.gt", "DensePull VertexParallel schedule Fig 9(b)", sssp_demo_dir + "sssp_DensePull_VertexParallel_schedule.cpp")
+
     
-    # Fig 6(c) Schedule with Paralleization Configured
-    compile_and_myprint("parallel_schedule.gt", "Schedule with Parallel Configured Fig 6(c)", pagerank_delta_demo_dir + "pagerankdelta_parallel_schedule.cpp")
+    # Fig 9(c) Schedule with Eager Bucket Update
+    compile_and_myprint("priority_update_eager_no_merge.gt", "Eager Bucket Update with SparsePush  Fig 9(c), code is in runtime library OrderedProcessingOperator", sssp_demo_dir + "sssp_eager_schedule.cpp")
 
-    # Fig 6(d) Schedule with Datalayout Configured 
-    compile_and_myprint("datalayout_schedule.gt", "Schedule with Datalayout Configured Fig 6(d)", pagerank_delta_demo_dir + "pagerankdelta_datalayout_schedule.cpp")
+    # Even more complicated schedule: with Eager Bucket Fusion Optimization
+    compile_and_myprint("priority_update_eager_with_merge.gt", "Eager Bucket Fusion with SparsePush (not included in the figure due to space constraint)", sssp_demo_dir + "pagerankdelta_cache_schedule.cpp")
 
-    # Even more complicated schedule: Schedule with Cache Optimization Configured (2 segments)
-    compile_and_myprint("cache_schedule.gt", "Schedule with Cache Optimized Configured (not included in the figure due to space constraint)", pagerank_delta_demo_dir + "pagerankdelta_cache_schedule.cpp")
-
-    myprint("Reproducing Fig 6 Successful! The generated C++ files are stored in the current directory")
+    myprint("Reproducing Fig 9 Successful! The generated C++ files are stored in the current directory")
 
 if __name__ == '__main__':
     eval_schedules()
