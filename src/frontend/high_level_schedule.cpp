9e6cc976a7c00f9d139d1e95e077baa9e1128fe8
tugsuu <tugsuu@mit.edu>
2020-01-09 05:09:05 -0500

    Fix small bug with intersection schedules

--- src/frontend/high_level_schedule.cpp
+++ src/frontend/high_level_schedule.cpp
@@ -624,9 +624,11 @@ namespace graphit {
             else if (intersection_option == "BinarySearchIntersection") {
                 (*schedule_->intersection_schedules)[intersection_label] = IntersectionSchedule::IntersectionType::BINARY;
             }
-
+            else if (intersection_option == "NaiveIntersection") {
+                (*schedule_->intersection_schedules)[intersection_label] = IntersectionSchedule::IntersectionType::NAIVE;
+            }
             else {
-                std::cout << "unsupported intersection: " << intersection_option << std::endl;
+                std::cout << "unsupported intersection: " << intersection_option << " using naive version instead" << std::endl;
                 (*schedule_->intersection_schedules)[intersection_label] = IntersectionSchedule::IntersectionType::NAIVE;
             }
 
