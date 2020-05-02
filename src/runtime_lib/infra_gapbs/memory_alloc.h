ee1e13aedfdc6304db908ef819c34efe4f58d407
tugsuu <tugsuu@mit.edu>
2020-05-02 17:01:24 -0400

    Add vector header

--- src/runtime_lib/infra_gapbs/memory_alloc.h
+++ src/runtime_lib/infra_gapbs/memory_alloc.h
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <type_traits>
 #include <map>
+#include <vector>
 #include <thread>
 #include <mutex>
 
@@ -18,36 +19,34 @@
 
 #include "infra_ligra/ligra/parallel.h"
 
-#include "segmentgraph.h"
 #include <memory>
 #include <assert.h>
-
 
 
 class MemAlloc {
 
 public:
 
-    MemAlloc(int64_t numNodes) : num_nodes_(numNodes){}
+    MemAlloc(int64_t numNodes) : num_nodes_(numNodes) {}
 
     ~MemAlloc() {
-        for (int i = 0; i < deduplication_flags.size();i++) {
+        for (int i = 0; i < deduplication_flags.size(); i++) {
             delete[] deduplication_flags[i];
         }
         deduplication_flags.clear();
     }
 
-    inline int* get_flags_atomic_() {
+    inline int *get_flags_atomic_() {
         static std::mutex thread_mutex;
         std::lock_guard<std::mutex> lock(thread_mutex);
 
         if (deduplication_flags.size() == 0) {
 
             deduplication_flags.push_back(new int[num_nodes_]);
-            ligra::parallel_for_lambda(0, (int)num_nodes_, [&] (int i) { deduplication_flags.back()[i]=0; });
+            ligra::parallel_for_lambda(0, (int) num_nodes_, [&](int i) { deduplication_flags.back()[i] = 0; });
         }
 
-        int * to_return = deduplication_flags.back();
+        int *to_return = deduplication_flags.back();
 
         deduplication_flags.pop_back();
 
@@ -55,7 +54,7 @@ public:
 
     }
 
-    inline void return_flags_atomic_(int * flags) {
+    inline void return_flags_atomic_(int *flags) {
 
         static std::mutex thread_mutex;
         std::lock_guard<std::mutex> lock(thread_mutex);
@@ -64,9 +63,8 @@ public:
     }
 
 
-
 private:
-    std::vector<int*> deduplication_flags;
+    std::vector<int *> deduplication_flags;
     int64_t num_nodes_;
 };
 
